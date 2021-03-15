9d436a904446d2de827908059bbc7b1f7da8a985
kernitus <2789734+kernitus@users.noreply.github.com>
2021-03-15 14:21:34 +0000

    Add Gradle build file

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+dependencies {
+    implementation("com.beust:klaxon:5.5")
+    testImplementation("io.kotest:kotest-runner-junit5:4.6.0")
+}
