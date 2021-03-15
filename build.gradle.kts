aa01fee6b40d45c4320249a8deb7d0438870477e
kernitus <2789734+kernitus@users.noreply.github.com>
2021-03-15 16:52:40 +0000

    Add Jar build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,18 @@ dependencies {
     implementation("com.beust:klaxon:5.5")
     testImplementation("io.kotest:kotest-runner-junit5:4.3.1")
 }
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Main-Class"] = "main.kotlin.FlattenerKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
+}
